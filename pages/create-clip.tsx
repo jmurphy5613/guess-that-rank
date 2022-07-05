537ce03f250e23098485af89e47565cf177a3926
jmurphy5613 <jmurphy5613@gmail.com>
2022-07-04 22:54:35 -0400

    added select for ranks

--- pages/create-clip.tsx
+++ pages/create-clip.tsx
@@ -16,10 +16,39 @@ const platformOptions = [
     {value: 'youtube', label: 'Youtube'}
 ]
 
+const rankOptions = [
+    {value: 'iron1', label: 'Iron 1'},
+    {value: 'iron2', label: 'Iron 2'},
+    {value: 'iron3', label: 'Iron 3'},
+    {value: 'brronze1', label: 'Bronze 1'},
+    {value: 'brronze2', label: 'Bronze 2'},
+    {value: 'brronze3', label: 'Bronze 3'},
+    {value: 'silver1', label: 'Silver 1'},
+    {value: 'silver2', label: 'Silver 2'},
+    {value: 'silver3', label: 'Silver 3'},
+    {value: 'gold1', label: 'Gold 1'},
+    {value: 'gold2', label: 'Gold 2'},
+    {value: 'gold3', label: 'Gold 3'},
+    {value: 'plat1', label: 'Platinum 1'},
+    {value: 'plat2', label: 'Platinum 2'},
+    {value: 'plat3', label: 'Platinum 3'},
+    {value: 'diamond1', label: 'Diamond 1'},
+    {value: 'diamond2', label: 'Diamond 2'},
+    {value: 'diamond3', label: 'Diamond 3'},
+    {value: 'ascendant1', label: 'Ascendant 1'},
+    {value: 'ascendant2', label: 'Ascendant 2'},
+    {value: 'ascendant3', label: 'Ascendant 3'},
+    {value: 'immortal1', label: 'Immortal 1'},
+    {value: 'immortal2', label: 'Immortal 2'},
+    {value: 'immortal3', label: 'Immortal 3'},
+    {value: 'radiant', label: 'Radiant'}
+]
+
 const CreateClip = () => {
 
     const [gameSelectedOption, setGameSelctionOption] = useState("");
     const [platformSelectedOption, setPlatformSelectedOption] = useState("");
+    const [rankOption, setRankOption] = useState("");
 
     const router = useRouter();
 
@@ -43,6 +72,11 @@ const CreateClip = () => {
                 className={styles.select}
                 options={platformOptions}
                 onChange={setPlatformSelectedOption}
+            />
+            <Select 
+                className={styles.select}
+                options={rankOptions}
+                onChange={setRankOption}
             />
             <input className={styles["url-input"]} placeholder={"Clip URL"} />
             <button className={styles.submit}>Submit</button>
