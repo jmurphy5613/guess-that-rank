5a0cfc1a559cb0c8fd54d72e99485266d94f8a07
jmurphy5613 <jmurphy5613@gmail.com>
2022-07-12 19:41:13 -0400

    more ga stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import ReactGa from 'react-ga';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    ReactGa.initialize('UA-234221342-1');
+    ReactGa.pageview(useRouter().pathname);
+  }, [])
+
   return(
     <UserProvider>
       <Component {...pageProps} />
