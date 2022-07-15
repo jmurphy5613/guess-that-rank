c3b3ce9581762660cb05276f0f5beed229bcc84f
jmurphy5613 <jmurphy5613@gmail.com>
2022-07-14 21:24:24 -0400

    changed favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return(
     <UserProvider>
+      <Head>
+        <title>Guess That Rank</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/LogoforJohn.jpeg" />
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   )
